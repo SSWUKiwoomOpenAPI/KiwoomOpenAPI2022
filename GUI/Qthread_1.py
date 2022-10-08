22950b6f845f62bfae57249852b67079c5be26c8
ohyukyoung <oouk1@naver.com>
2022-10-08 23:00:06 +0900

    Qthread_1

--- GUI/Qthread_1.py
+++ GUI/Qthread_1.py
@@ -3,7 +3,6 @@ from kiwoom import Kiwoom          # 로그인을 위한 클래스
 from PyQt5.QtWidgets import *      #PyQt import
 
 
-
 class Thread1(QThread):
     def __init__(self, parent):   # 부모의 윈도우 창을 가져올 수 있다.
         super().__init__(parent)  # 부모의 윈도우 창을 초기화 한다.
@@ -72,11 +71,12 @@ class Thread1(QThread):
 
             #################################### 텍스트 라벨에 집어 넣기
 
-            self.parent.label_1.setText(str(format(totalBuyingPrice, ",")))
-            self.parent.label_2.setText(str(format(currentTotalPrice, ",")))
-            self.parent.label_3.setText(str(format(balanceAsset, ",")))
-            self.parent.label_4.setText(str(format(totalEstimateProfit, ",")))
-            self.parent.label_5.setText(str(format(total_profit_loss_rate, ",")))
+            self.parent.label_1.setText(str(totalBuyingPrice))
+            self.parent.label_2.setText(str(currentTotalPrice))
+            self.parent.label_3.setText(str(balanceAsset))
+            self.parent.label_4.setText(str(totalEstimateProfit))
+            self.parent.label_5.setText(str(total_profit_loss_rate))
+
             #################################################################
 
 
@@ -108,12 +108,11 @@ class Thread1(QThread):
 
                 self.parent.stocklistTableWidget_2.setItem(index, 0, QTableWidgetItem(str(itemCode)))
                 self.parent.stocklistTableWidget_2.setItem(index, 1, QTableWidgetItem(str(itemName)))
-                # 천단위 구분 콤마 사용
-                self.parent.stocklistTableWidget_2.setItem(index, 2, QTableWidgetItem(str(format(amount, ","))))
-                self.parent.stocklistTableWidget_2.setItem(index, 3, QTableWidgetItem(str(format(buyingPrice, ","))))
-                self.parent.stocklistTableWidget_2.setItem(index, 4, QTableWidgetItem(str(format(currentPrice, ","))))
-                self.parent.stocklistTableWidget_2.setItem(index, 5, QTableWidgetItem(str(format(estimateProfit, ","))))
-                self.parent.stocklistTableWidget_2.setItem(index, 6, QTableWidgetItem(str(format(profitRate, ","))))
+                self.parent.stocklistTableWidget_2.setItem(index, 2, QTableWidgetItem(str(amount)))
+                self.parent.stocklistTableWidget_2.setItem(index, 3, QTableWidgetItem(str(buyingPrice)))
+                self.parent.stocklistTableWidget_2.setItem(index, 4, QTableWidgetItem(str(currentPrice)))
+                self.parent.stocklistTableWidget_2.setItem(index, 5, QTableWidgetItem(str(estimateProfit)))
+                self.parent.stocklistTableWidget_2.setItem(index, 6, QTableWidgetItem(str(profitRate)))
 
             if sPrevNext == "2":
                 self.detail_acount_mystock(sPrevNext="2")  # 다음 페이지가 있으면 전부 검색한다.
