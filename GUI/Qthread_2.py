37bb464fd731490c61a3dc8e50c71c01ccd681ba
yun0011 <hiyun01@naver.com>
2022-12-12 17:42:17 +0900

    최종수정

--- GUI/Qthread_2.py
+++ GUI/Qthread_2.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *        # PyQt import
 from PyQt5.QtTest import *           # 시간관련 함수
 from datetime import datetime, timedelta    # 특정 일자를 조회
 
-class Thread2(Qthread):
+class Thread2(QThread):
     def __init__(self, parent):     # 부모의 윈도운 창을 가져올 수 있다.
         super().__init__(parent)    # 부모 윈도우 창을 초기화
         self.parent = parent
@@ -15,7 +15,7 @@ class Thread2(Qthread):
         self.Find_down_Screen = "1200"  # 계좌평가잔고내역을 받기위한 스크린
         self.code_in_all = None  # 1600개 코드 중 1개 코드, 쌓이지 않고 계속 갱신
 
-        self.Predict_Screen = "1400"
+        self.Predic_Screen = "1400"
         self.calcul_data = []
         self.second_filter = []
         self.Predic_start = []
@@ -50,24 +50,26 @@ class Thread2(Qthread):
             self.parent.Danger_wd.setItem(index2, 2, QTableWidgetItem(self.k.acc_portfolio[k]["위험도"]))
             index2 += 1
 
-        def Invers_arrangement(self):
-            code_list =[]
-            for code in self.k.acc_portfolio.keys():
-                code_list.append(code)
+    def Invers_arrangement(self):
 
-            print("계좌포함 종목 %s" % (code_list))
+        code_list = []
+        for code in self.k.acc_portfolio.keys():
+            code_list.append(code)
 
-            for idx, code in enumerate(code_list):
-              QTest.qWait(1000)
+        print("계좌포함 종목 %s" % (code_list))
 
-              self.code_in_all = code  # 종목코드 선언
-              print("%s 종목 검사 중 코드이름 : %s." % (idx + 1, self.code_in_all))
+        for idx, code in enumerate(code_list):
+            QTest.qWait(1000)
 
-              self.k.kiwoom.dynamicCall("DisconnectRealData(QString)", self.Predic_Screen)  # 해당 스크린을 끊고 다시 시작
-              self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
-              self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1") # 수정주가구분 0: 액면분할등이 포함되지 않음, 1: 포함됨
-              self.k.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "주식일봉차트조회", "opt10081", "0", self.Predic_Screen)
-              self.detail_account_info_event_loop.exec_()
+            self.code_in_all = code  # 종목코드 선언 (중간에 코드 정보 받아오기 위해서)
+
+            print("%s 종목 검사 중 코드이름 : %s." % (idx + 1, self.code_in_all))
+
+            self.k.kiwoom.dynamicCall("DisconnectRealData(QString)", self.Predic_Screen)  # 해당 스크린을 끊고 다시 시작
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1") # 수정주가구분 0: 액면분할등이 포함되지 않음, 1: 포함됨
+            self.k.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "주식일봉차트조회", "opt10081", "0", self.Predic_Screen)
+            self.detail_account_info_event_loop.exec_()
 
 
         
